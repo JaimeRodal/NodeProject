9670dc8963710496fd161808a5dbfb429fe18a03
Hugo Otero <hugootero94@gmail.com>
2023-12-06 23:27:33 +0100

    Tablas DB actualizadas

--- src/db/initDb.js
+++ src/db/initDb.js
@@ -5,51 +5,89 @@ const create = async () => {
     let pool = await getPool();
 
     console.log("Borrando tablas...");
-    await pool.query("DROP TABLE IF EXISTS photos,experiences,locations,users");
+    await pool.query(
+      "DROP TABLE IF EXISTS votes, answerComments, comments, experiences, categories, users"
+    );
 
     console.log("Creando tabla users...");
     await pool.query(`
         CREATE TABLE IF NOT EXISTS users (
-            id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(64) UNIQUE,
-            email VARCHAR(255) UNIQUE,
-            password VARCHAR(64) NOT NULL,
-            profilePictureURL VARCHAR(255)
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+            lastName VARCHAR(50) NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(50) NOT NULL,
+            photo VARCHAR(200),
+            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updatedAt DATETIME ON UPDATE CURRENT_TIMESTAMP            
         )
         `);
 
-    console.log("Creando tabla locations...");
+    console.log("Creando tabla categorías...");
     await pool.query(`
-        CREATE TABLE IF NOT EXISTS locations (
-            id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            streetAddress VARCHAR(255),
-            subLocality VARCHAR(255),
-            city VARCHAR(255) NOT NULL,
-            country VARCHAR(255) NOT NULL
-        )
-        `);
+        CREATE TABLE IF NOT EXISTS categories (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updatedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
+        )`);
 
     console.log("Creando tabla experiences...");
     await pool.query(`
         CREATE TABLE IF NOT EXISTS experiences (
-            id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            description TEXT NOT NULL,
-            locationId INT UNSIGNED NOT NULL,
-            ownerId INT UNSIGNED NOT NULL,
-            publishedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            
-            FOREIGN KEY (locationId) REFERENCES locations(id),
-            FOREIGN KEY (ownerId) REFERENCES users(id)
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            title VARCHAR(50) NOT NULL,
+            subTitle VARCHAR(50) NOT NULL,
+            place VARCHAR(50) NOT NULL,
+            text VARCHAR(500) NOT NULL,
+            photo VARCHAR(50),
+            user_id INT NOT NULL,
+            category_id INT NOT NULL,
+            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updatedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
+            FOREIGN KEY(user_id) REFERENCES users(id),
+            FOREIGN KEY(category_id) REFERENCES categories(id)
         )
         `);
-    console.log("Creando tabla photos...");
+
+    console.log("Creando tabla commentarios...");
     await pool.query(`
-        CREATE TABLE IF NOT EXISTS photos (
-            id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            url VARCHAR(255) NOT NULL,
-            experienceId INT UNSIGNED NOT NULL,
-            
-            FOREIGN KEY (experienceId) REFERENCES experiences(id)
+        CREATE TABLE IF NOT EXISTS comments (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            text VARCHAR(200) NOT NULL,
+            exp_id INT NOT NULL,
+            user_id INT NOT NULL,
+            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updatedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
+            FOREIGN KEY(exp_id) REFERENCES experiences(id),
+            FOREIGN KEY(user_id) REFERENCES users(id)
+        )
+        `);
+
+    console.log("Creando tabla respuestas de comentarios...");
+    await pool.query(`
+            CREATE TABLE IF NOT EXISTS answerComments (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                text VARCHAR(200) NOT NULL,
+                comment_id INT NOT NULL,
+                user_id INT NOT NULL,
+                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+                updatedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
+                FOREIGN KEY(comment_id) REFERENCES comments(id),
+                FOREIGN KEY(user_id) REFERENCES users(id)
+            )
+            `);
+
+    console.log("Creando tabla votos...");
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS votes (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            user_id INT NOT NULL,
+            exp_id INT NOT NULL,
+            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updatedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
+            FOREIGN KEY(exp_id) REFERENCES experiences(id),
+            FOREIGN KEY(user_id) REFERENCES users(id)
         )
         `);
 
