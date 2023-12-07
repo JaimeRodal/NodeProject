1ef1460619f91b1994196ba4d28f6f40f1f06a07
Jaime Rodal <jarodalr@gmail.com>
2023-12-07 18:29:32 +0100

    meter foto de perfil

--- src/controllers/users/deleteController.js
+++ src/controllers/users/deleteController.js
@@ -1,13 +1,10 @@
 import getPool from "../../db/getPool.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import genError from "../../utils/helpers.js";
 
 const pool = await getPool();
 
 const deleteController = async (req, res, next) => {
   try {
-    console.log(req.auth);
     const { id } = req.params;
     const [[user]] = await pool.query(
       `
@@ -15,8 +12,6 @@ const deleteController = async (req, res, next) => {
     `,
       [id]
     );
-    console.log(id);
-    console.log(user);
     if (req.auth !== user.id) {
       throw genError("No puedes borrar el perfíl de otro usuario", 401);
     }
--- src/controllers/users/registerController.js
+++ src/controllers/users/registerController.js
@@ -1,43 +1,68 @@
+import express from "express";
+import fileUpload from "express-fileupload";
 import getPool from "../../db/getPool.js";
 import bcrypt from "bcrypt";
 import sendMailUtil from "../../utils/sendMail.js";
 import genError from "../../utils/helpers.js";
 
+const app = express();
 const pool = await getPool();
 
-const register = async (req, res, next) => {
+// Middleware para poder subir archivos
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
+
+app.post("/register", async (req, res, next) => {
   try {
     const { name, lastName, email, password } = req.body;
+
+    // Verificar si se cargó un archivo
+    if (!req.files || !req.files.avatar) {
+      throw genError("Debes subir un archivo", 400);
+    }
+
+    // Obtener el archivo de la solicitud
+    const avatar = req.files.avatar;
+
+    // Verificar si el email ya está en uso
     const [[emailExists]] = await pool.query(
       `SELECT * FROM users WHERE email=?`,
       [email]
     );
 
     if (emailExists) {
-      throw genError("El email ya esta en uso", 400);
+      throw genError("El email ya está en uso", 400);
     }
 
-    // Hasheando el password
-
+    // Hashear la contraseña
     const hashedPass = await bcrypt.hash(password, 5);
+
+    // Guardar la imagen en la carpeta "uploads"
+    const nombreArchivoFinal = Date.now() + "-" + avatar.name;
+    avatar.mv(`./uploads/${nombreArchivoFinal}`);
+
+    // Insertar el nuevo usuario en la base de datos con la ruta de la imagen de perfil
     await pool.query(
-      `INSERT INTO users (name, lastName, email,password) VALUES (?,?,?,?)`,
-      [name, lastName, email, hashedPass]
+      `INSERT INTO users (name, lastName, email, password, photo) VALUES (?, ?, ?, ?, ?)`,
+      [name, lastName, email, hashedPass, `../../uploads/${nombreArchivoFinal}`]
     );
 
-    //Nodemailer para los que se registren
+    // Nodemailer para los que se registran
     const emailSubject = "Cuenta registrada";
-    const emailBody = `<h1> Bienvenid@ ${name}! 😄
-    Esperamos que encuentre los que busca y tenga una experiencia positiva 😎`;
+    const emailBody = `<h1> ¡Bienvenid@ ${name}! 😄 Esperamos que encuentres lo que buscas y tengas una experiencia positiva 😎`;
 
-    // Hacemos la funcion para mandar el mail
+    // Enviar el correo electrónico
     await sendMailUtil(email, emailSubject, emailBody);
+
     res.status(200).json({
-      message: "Usuario creado con exito!",
+      message: "Usuario creado con éxito!",
     });
   } catch (error) {
     next(error);
   }
-};
+});
 
-export default register;
+export default app;
