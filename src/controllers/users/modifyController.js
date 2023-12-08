cfbbd46397ae61a02bd5d2b50279daf7580003b9
Jaime Rodal <jarodalr@gmail.com>
2023-12-08 20:33:48 +0100

    mod user arreglado

--- src/controllers/users/modifyController.js
+++ src/controllers/users/modifyController.js
@@ -3,85 +3,89 @@ import fileUpload from "express-fileupload";
 import getPool from "../../db/getPool.js";
 import bcrypt from "bcrypt";
 import genError from "../../utils/helpers.js";
+import Auth from "../../middlewares/auth.js";
 
-const app = express();
+const modifyApp = express();
 const pool = await getPool();
 
-//Usando express para subir alguna foto si quieren modificarla
+// Middleware para poder subir archivos
+modifyApp.use(fileUpload({ createParentPath: true }));
 
-app.use(fileUpload({ createParentPath: true }));
-
-//Funcion para modificar los datos
+// Función para modificar los datos
 const modify = async (req, res, next) => {
   try {
-    //Guardamos el id del usuario autenticado con el token en una constante
+    // Guardamos el id del usuario autenticado con el token en una constante
     const authUserId = req.auth;
-    //Guardamos el id del usuario del que quiere modificar los datos
+    console.log(authUserId);
+
+    // Guardamos el id del usuario del que quiere modificar los datos
     const modUser = req.params.id;
-    //Comprobamos que los 2 ids coinciden, si no es asi lanza un error
-    if (authUserId !== modUser) {
+    console.log(modUser);
+
+    // Comprobamos que los 2 ids coinciden, si no es así lanza un error
+    if (String(authUserId) !== String(modUser)) {
       throw genError("No tienes permisos para editar este usuario", 403);
     }
 
-    //Guardamos la respuesta del body con los datos que nos interesa poder modificar
+    // Guardamos la respuesta del body con los datos que nos interesa poder modificar
     const { name, lastName, email, password } = req.body;
 
-    //Verificamos que hayan querido actualizar la foto
+    // Verificamos si hay un archivo para actualizar la foto
     let photoPath = null;
     if (req.files && req.files.avatar) {
       const avatar = req.files.avatar;
       const finalFileName = Date.now() + "-" + avatar.name;
-      avatar.mv(`../../uploads/${finalFileName}`);
+      avatar.mv(`./uploads/${finalFileName}`);
       photoPath = `../../uploads/${finalFileName}`;
     }
 
-    //Hasheamos la pass si se pide
+    // Hasheamos la contraseña si se proporciona
     let hashedPass = null;
     if (password) {
       hashedPass = await bcrypt.hash(password, 5);
     }
 
-    //Aqui incluimos la consulta a la base de datos para actualizar los valores
+    // Aquí incluimos la consulta a la base de datos para actualizar los valores
     let updateQuery = "UPDATE users SET";
     const values = [];
 
     if (name) {
-      updateQuery += "name=?, ";
+      updateQuery += " name=?,";
       values.push(name);
     }
 
     if (lastName) {
-      updateQuery += "lastName=?, ";
+      updateQuery += " lastName=?,";
       values.push(lastName);
     }
 
     if (email) {
-      updateQuery += "email=?, ";
+      updateQuery += " email=?,";
       values.push(email);
     }
 
     if (hashedPass) {
-      updateQuery += "password=?, ";
+      updateQuery += " password=?,";
       values.push(hashedPass);
     }
 
     if (photoPath) {
-      updateQuery += "photo=?, ";
+      updateQuery += " photo=?,";
       values.push(photoPath);
     }
 
-    //Eliminamos la coma del query de la ultima actualizacion que se haga
-    updateQuery = updateQuery.slice(0, -2);
+    // Eliminamos la coma del query de la última actualización que se haga
+    updateQuery = updateQuery.slice(0, -1);
 
-    //Metemos la ultima query para que se cambie solo al usuario con ese id
+    // Agregamos la condición WHERE para el usuario específico
     updateQuery += " WHERE id=?";
-    values.push(id);
+    values.push(modUser);
 
-    //Ejecutamos la actualizacion
+    // Ejecutamos la actualización
     await pool.query(updateQuery, values);
 
     res.status(200).json({
-      message: "Usuario modificado!",
+      message: "Usuario modificado con éxito!",
     });
   } catch (error) {
     next(error);
