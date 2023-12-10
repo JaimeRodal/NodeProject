09e9a9a9677269d486d78a087ea4ca3f1cfd1ccf
charlie-snow <victo@victoriano.none>
2023-12-10 21:15:09 +0100

    insertExperience no recibe los parámetros del form

--- src/models/getExperience.js
+++ src/models/getExperience.js
@@ -34,10 +34,19 @@ const insertExperience = async ({
       loggedUserId,
       category_id,
     ];
-    console.log(sqlValues);
 
     console.log("Query: " + mysql.format(sqlQuery, sqlValues));
-    const [{ insertId }] = await pool.query(sqlQuery, sqlValues);
+    const [{ insertId }] = await pool.query(
+      `INSERT INTO experiences (
+        title,
+        subtitle,
+        place,
+        text,
+        photo,
+        user_id,
+        category_id) VALUES (?,?,?,?,?,?,?)`,
+      [title, subtitle, place, text, photo, loggedUserId, category_id]
+    );
     return insertId;
   } catch (error) {
     // throw new Error("Error inserting experience: " + error);
