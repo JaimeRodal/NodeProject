2ec18c43bb89417cc6b1380aa5d737187d62c42d
charlie-snow <victo@victoriano.none>
2023-12-14 10:49:39 +0100

    nada

--- src/controllers/schemas/users/loginUser.js
+++ src/controllers/schemas/users/loginUser.js
@@ -2,13 +2,13 @@ import Joi from "joi";
 
 // Crear el esquema del Login como un objeto Joi
 const loginSchema = Joi.object({
+  // Definimos los parámetros que se validarán con las caracteríscticas que requiramos
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net", "es"] },
+  }),
 
-// Definimos los parámetros que se validarán con las caracteríscticas que requiramos
-email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'es'] } }),
-
-password: Joi.string().required(),
-
+  password: Joi.string().required(),
 });
 // Exportamos el Schema
-export default loginSchema; 
+export default loginSchema;
