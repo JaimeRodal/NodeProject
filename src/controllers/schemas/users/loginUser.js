2494f8a89214f9d2066c3160c994ff358e595f75
charlie-snow <victo@victoriano.none>
2023-12-14 15:15:20 +0100

    Revisión de validaciones

--- src/controllers/schemas/users/loginUser.js
+++ src/controllers/schemas/users/loginUser.js
@@ -3,12 +3,17 @@ import Joi from "joi";
 // Crear el esquema del Login como un objeto Joi
 const loginSchema = Joi.object({
   // Definimos los parámetros que se validarán con las caracteríscticas que requiramos
-  email: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: { allow: ["com", "net", "es"] },
-  }),
+  // email: Joi.string().email({    minDomainSegments: 2,    tlds: { allow: ["com", "net", "es"] },  }),
 
-  password: Joi.string().required(),
+  // NOTA: La verificación estándar es la simple, ya que ésta dejaría fuera algunos correos, como los .org
+  email: Joi.string().email().required(),
+
+  // password: Joi.string().required(),
+
+  // NOTA: Para evitar caracteres extraños o que puedan dar problemas, limitamos el password a letras, números y caracteres especiales
+  password: Joi.string()
+    .pattern(/^[a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]+$/)
+    .required(),
 });
 // Exportamos el Schema
 export default loginSchema;
