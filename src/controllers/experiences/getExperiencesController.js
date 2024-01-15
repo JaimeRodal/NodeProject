e1cafc16060ac1741e919998846a29243ba2f0c9
Hugo Otero <hugootero94@gmail.com>
2024-01-15 18:09:11 +0100

    reformulada peticion SQL

--- src/controllers/experiences/getExperiencesController.js
+++ src/controllers/experiences/getExperiencesController.js
@@ -1,57 +1,65 @@
 // Importaciones
 import getPool from "../../db/getPool.js";
+import genError from "../../utils/helpers.js";
 
 // Importamos e usamos el gestor de conexión a la DB
 const pool = await getPool();
 
-// Creamos una función para listar experiencias con opciones de filtrado donde busca y extrae las coincidencias en las experiencias publicadas buscando en lugares y categorías, y es capaz de orodenar por votos, según los parámetros introducidos
+// Creamos una función de filtrado donde busca y extrae las coincidencias en las experiencias publicadas buscando en lugares y categorías
 const getExperiencesController = async (req, res, next) => {
   try {
-    // declaramos las variables con partes de sql para hacer combinaciones
-    let query = "";
-    const query_select = `SELECT 
-    exp.title,
-    exp.subTitle,
-    exp.place,
-    exp.text,
-    exp.photo,
-    cat.name AS category_name,
-    COALESCE(COUNT(v.exp_id), 0) AS likes,
-    u.name AS user_name,
-    u.photo AS user_photo,
-    CONCAT(
-        '[',
-        GROUP_CONCAT(
-            CONCAT(
-                '{"comment_text":"', c.text, '", "comment_user":"', cu.name, '", "answer_text":"', ans.text, '", "answer_user":"', au.name, '"}'
-            )
-            ORDER BY c.id, ans.id
-            SEPARATOR ','
-        ),
-        ']'
-    ) AS comments_and_answers
-        FROM 
-            experiences exp
-        JOIN 
-            categories cat ON exp.category_id = cat.id 
-        JOIN 
-            users u ON exp.user_id = u.id
-        LEFT JOIN 
-            votes v ON exp.id = v.exp_id
-        LEFT JOIN 
-            comments c ON exp.id = c.exp_id
-        LEFT JOIN 
-            answerComments ans ON c.id = ans.comment_id
-        LEFT JOIN 
-            users cu ON c.user_id = cu.id
-        LEFT JOIN 
-            users au ON ans.user_id = au.id`;
-    const query_where = `WHERE 
-            exp.place LIKE ? OR cat.name LIKE ? OR exp.title LIKE ? OR exp.subTitle LIKE ? OR exp.text LIKE ? OR u.name LIKE ?`;
-    const query_group = `GROUP BY
-            exp.title, exp.subTitle, exp.place, exp.text, exp.photo, cat.name, u.name, u.photo`;
-    const query_order = `ORDER BY 
-            likes `;
+    // Obtenemos en la variable list el tipo de listado ('search' o 'votes'), según sea una búsqueda o un listado por votos
+    const list = req.query.list;
+
+    if (list === "search") {
+      // Definimos en una variable el parametro de búsqueda recibido para su uso
+      const searchText = req.query.text;
+      // Creamos una petición a la DB pasándole el parámetro anterior
+      const [filter] = await pool.query(
+        `
+        SELECT 
+          exp.title,
+          exp.subTitle,
+          exp.place,
+          exp.text,
+          exp.photo,
+          cat.name AS category_name,
+          COALESCE(COUNT(v.exp_id), 0) AS likes,
+          u.name AS user_name,
+          u.photo AS user_photo,
+          CONCAT(
+              '[',
+              GROUP_CONCAT(
+                  CONCAT(
+                      '{"comment_text":"', c.text, '", "comment_user":"', cu.name, '", "answer_text":"', ans.text, '", "answer_user":"', au.name, '"}'
+                  )
+                  ORDER BY c.id, ans.id
+                  SEPARATOR ','
+              ),
+              ']'
+          ) AS comments_and_answers
+              FROM 
+                  experiences exp
+              JOIN 
+                  categories cat ON exp.category_id = cat.id 
+              JOIN 
+                  users u ON exp.user_id = u.id
+              LEFT JOIN 
+                  votes v ON exp.id = v.exp_id
+              LEFT JOIN 
+                  comments c ON exp.id = c.exp_id
+              LEFT JOIN 
+                  answercomments ans ON c.id = ans.comment_id
+              LEFT JOIN 
+                  users cu ON c.user_id = cu.id
+              LEFT JOIN 
+                  users au ON ans.user_id = au.id
+              WHERE 
+                  exp.place LIKE ? OR cat.name LIKE ? OR exp.title LIKE ? OR exp.subTitle LIKE ? OR exp.text LIKE ? OR u.name LIKE ?
+              GROUP BY
+                  exp.title, exp.subTitle, exp.place, exp.text, exp.photo, cat.name, u.name, u.photo
+              ORDER BY 
+                  exp.createdAt DESC;
 
     // hay 2 opciones principales: lista simple o lista filtrada
     const query_list = query_select + "\n" + query_group;
@@ -63,41 +71,104 @@ const getExperiencesController = async (req, res, next) => {
     const orderBy = req.query.orderBy;
     const orderDirection = req.query.orderDirection;
 
-    // si se quiere ordenar por votos, se añade el order by al final de la query
-    if (orderBy === "votes" && orderDirection) {
-      query = query + "\n" + query_order;
-      if (orderDirection === "ASC") {
-        query = query + "ASC";
-      } else if (orderDirection === "DESC") {
-        query = query + "DESC";
-      }
-    }
 
-    // si se va a filtrar por un texto, se elige la lista filtrada, se realiza la búsqueda y se devuelve el resultado
-    if (search) {
-      query = query_filtered_list + "\n" + query;
-      const [list] = await pool.query(query, [
-        `%${search}%`,
-        `%${search}%`,
-        `%${search}%`,
-        `%${search}%`,
-        `%${search}%`,
-        `%${search}%`,
-      ]);
-      console.log(list);
+
+    `,
+        [
+          `%${searchText}%`,
+          `%${searchText}%`,
+          `%${searchText}%`,
+          `%${searchText}%`,
+          `%${searchText}%`,
+          `%${searchText}%`,
+        ]
+      );
+
+      // En caso de no haber ninguna coincidencia de búsqueda genera mensaje de error
+      if (!filter || filter.length === 0) {
+        throw genError("No hay ninguna coincidencia", 401);
+      }
+
+      // Respuesta
       res.send({
         status: "Correcto",
-        data: list,
+        data: filter,
       });
-    }
-    // si no se va a filtrar por un texto, se elige la lista simple, se realiza la búsqueda y se devuelve el resultado
-    else {
-      query = query_list + "\n" + query;
-      const [list] = await pool.query(query);
-      console.log(list);
+    } else if (list === "votes") {
+      let order = req.query.order;
+
+      // Comprobamos que el parámetro pasado coincida con alguna de las dos necesarias para realizar la acción correctamente, de no ser así, generamos un error
+      if (order !== "DESC" && order !== "ASC") {
+        throw genError("Los valores introducidos han de ser ASC o DESC", 400);
+      }
+
+      // Creamos la query de SQL pasándole el parámetro adecuado a la petición
+      const sqlQuery = `
+      SELECT 
+          exp.title,
+          exp.subTitle,
+          exp.place,
+          exp.text,
+          exp.photo,
+          cat.name AS category_name,
+          COALESCE(COUNT(v.exp_id), 0) AS likes,
+          u.name AS user_name,
+          u.photo AS user_photo,
+          CONCAT(
+              '[',
+              GROUP_CONCAT(
+                  CONCAT(
+                      '{"comment_text":"', c.text, '", "comment_user":"', cu.name, '", "answers": ',
+                      COALESCE(
+                          (SELECT 
+                              CONCAT(
+                                  '[',
+                                  GROUP_CONCAT(
+                                      CONCAT(
+                                          '{"answer_text":"', ans.text, '", "answer_user":"', au.name, '"}'
+                                      )
+                                      ORDER BY ans.id
+                                      SEPARATOR ','
+                                  ),
+                                  ']'
+                              )
+                          FROM answerComments ans
+                          JOIN users au ON ans.user_id = au.id
+                          WHERE ans.comment_id = c.id),
+                          '[]'
+                      ),
+                      '}'
+                  )
+                  ORDER BY c.id
+                  SEPARATOR ','
+              ),
+              ']'
+          ) AS comments_and_answers
+          FROM 
+              experiences exp
+          JOIN 
+              categories cat ON exp.category_id = cat.id 
+          JOIN 
+              users u ON exp.user_id = u.id
+          LEFT JOIN 
+              votes v ON exp.id = v.exp_id
+          LEFT JOIN 
+              comments c ON exp.id = c.exp_id
+          LEFT JOIN 
+              users cu ON c.user_id = cu.id
+          GROUP BY
+              exp.title, exp.subTitle, exp.place, exp.text, exp.photo, cat.name, u.name, u.photo
+          ORDER BY 
+              likes ${order === "DESC" ? "DESC" : "ASC"};
+    `;
+
+      // Realizamos la petición a la DB
+      const [orderBy] = await pool.query(sqlQuery);
+
+      // Respuesta
       res.send({
         status: "Correcto",
-        data: list,
+        data: orderBy,
       });
     }
 
