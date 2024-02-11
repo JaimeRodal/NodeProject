7c8a04b6bc535537e233bc040345de48188c6991
Diego <diegolema31@gmail.com>
2024-02-11 20:23:02 +0100

    Correccion de experiencias de ejemplo

--- src/db/experiencesEx.js
+++ src/db/experiencesEx.js
@@ -33,16 +33,16 @@ const exp = [
     title: 'Aventura por el desierto', 
     subTitle: 'De viaje por Marruecos', 
     place: 'El desierto de Merzouga', 
-    text: 'El mes pasado nos fuimos a Marruecos de vacaciones, vimos muchísimos lugares increíbles. Empezamos visitando la plaza de Jemaa el-Fna, la plaza mas famosa de la ciudad, pero la verdadera aventura empezó cuando empezamos la ruta hacia el desierto de Merzouga, un lugar árido que pudimos explorar en un 4x4 para acabar pasando una noche bajo las estrellas en un campamento bereber, sin duda repetiremos!', 
+    text: 'El mes pasado nos fuimos a Marruecos de vacaciones, vimos muchísimos lugares increíbles. Empezamos visitando la plaza de Jemaa el-Fna, la plaza más famosa de la ciudad, pero la verdadera aventura empezó cuando empezamos la ruta hacia el desierto de Merzouga, un lugar árido que pudimos explorar en un 4x4 para acabar pasando una noche bajo las estrellas en un campamento bereber, sin duda repetiremos!', 
     photo: `http://${HOST_DB}:${PORT}/examples/desierto-mezouga.png`, 
     user_id: '1', 
     category_id: '1'
   },
   {
-    title: 'Paseo por el Río Sil', 
-    subTitle: 'Ribeira Sacra al natural', 
+    title: 'Ribeira Sacra al natural', 
+    subTitle: 'Paseo por el Río Sil', 
     place: 'Ribeira Sacra', 
-    text: 'La semana pasada vimimos un día increíble de excursión por la Ribeira Sacra. Empezamos en el embarcadero de San Estevo, donde tomamos un catamaran a lo largo del Río Sil. Vimos grandes extensiones de viñedos y acantilados casi verticales, un paisaje impresionante. Después visitamos el Mirador de San Estevo, con unas vistas espectaculares. Ya al mediodía llegamos a Allariz, en el nos dejaron tiempo para comer y después tuvimos una visita guiada por el pueblo y al atardecer volvimos a casa.', 
+    text: 'La semana pasada vivimos un día increíble de excursión por la Ribeira Sacra. Empezamos en el embarcadero de San Estevo, donde tomamos un catamarán a lo largo del Río Sil. Vimos grandes extensiones de viñedos y acantilados casi verticales, un paisaje impresionante. Después visitamos el Mirador de San Estevo, con unas vistas espectaculares. Ya al mediodía llegamos a Allariz, en él nos dejaron tiempo para comer y después tuvimos una visita guiada por el pueblo y al atardecer volvimos a casa.', 
     photo: `http://${HOST_DB}:${PORT}/examples/ribeira-sacra.png`, 
     user_id: '2', 
     category_id: '2'
@@ -51,7 +51,7 @@ const exp = [
     title: 'Viaje a Las Vegas', 
     subTitle: 'Esta vez sin resaca', 
     place: 'Las Vegas', 
-    text: 'Mis colegas y yo celebramos la despedida de soltero de mi mejor amigo Juan en Las Vegas, lo pasamos de miedo en los casinos, viendo algún espectáculo como el Circo del Sol y un show de magia. todo iba bien hasta que empezamos a perder en la ruleta, Juan quería seguir pero conseguimos llevárnoslo de allí, volvió a casa feliz pero sin un duro el pobre...', 
+    text: 'Mis colegas y yo celebramos la despedida de soltero de mi mejor amigo Juan en Las Vegas, lo pasamos de miedo en los casinos, viendo algún espectáculo como el Circo del Sol y un show de magia. Todo iba bien hasta que empezamos a perder en la ruleta, Juan quería seguir, pero conseguimos llevárnoslo de allí, volvió a casa feliz pero sin un duro el pobre...', 
     photo: `http://${HOST_DB}:${PORT}/examples/las-vegas.png`, 
     user_id: '3', 
     category_id: '3'
@@ -60,7 +60,7 @@ const exp = [
     title: 'Debut con  el sushi', 
     subTitle: 'Probando nuevas comidas', 
     place: 'A Coruña', 
-    text: 'Ayer estuve con mi pareja en el local Sushi Utopía, ha sido mi primera vez probando sushi, algo que me parecía muy mala idea. Empezamos probando los típicos nigiris de salmón y de atún, después probamos unos makis, seguidos de uramakis, que es lo mismo solo que uno tiene el arroz por dentro y otro por fuera. Para finalizar nos pedimos unos baos con unos uramaki de gamba en tempura con cebolla frita (mis favoritos). Al final fué una experiencia interesante, pero no la cambio por un churrasco...', 
+    text: 'Ayer estuve con mi pareja en el local Sushi Utopía, ha sido mi primera vez probando sushi, algo que me parecía muy mala idea. Empezamos probando los típicos nigiris de salmón y de atún, después probamos unos makis, seguidos de uramakis, que es lo mismo solo que uno tiene el arroz por dentro y otro por fuera. Para finalizar nos pedimos unos baos con unos uramaki de gamba en tempura con cebolla frita (mis favoritos). Al final fue una experiencia interesante, pero no la cambio por un churrasco...', 
     photo: `http://${HOST_DB}:${PORT}/examples/sushi.png`, 
     user_id: '4', 
     category_id: '4'
@@ -69,7 +69,7 @@ const exp = [
     title: 'Museo de Historia Natural', 
     subTitle: 'Paseo entre dinosaurios', 
     place: 'Londres', 
-    text: 'Fuí a visitar a mi prima a Londres y me hizo un pequeño tour por la ciudad, lo que más me gustó fué el Museo de historia Natural, es un lugar impresionante donde hai fósiles de dinosaurio, distintos esqueletos de mamíferos e incluso la recreación de una ballena azul a tamaño real en el techo, lo pasé genial, ojalá poder volver pronto!', 
+    text: 'Fui a visitar a mi prima a Londres y me hizo un pequeño tour por la ciudad, lo que más me gustó fue el Museo de historia Natural, es un lugar impresionante donde hay fósiles de dinosaurio, distintos esqueletos de mamíferos e incluso la recreación de una ballena azul a tamaño real en el techo, lo pasé genial, ojalá poder volver pronto!', 
     photo: `http://${HOST_DB}:${PORT}/examples/museo-historia-natural-londres.png`, 
     user_id: '5', 
     category_id: '5'
